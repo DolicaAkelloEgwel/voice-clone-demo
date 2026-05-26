05e75ab0fb7d3182cec0d7c7596b6f8c4626138f
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-05-26 15:51:38 +0100

    demo code

---
+++ main.py
@@ -0,0 +1,13 @@
+import torchaudio as ta
+from chatterbox.tts_turbo import ChatterboxTurboTTS
+
+# Load the Turbo model
+model = ChatterboxTurboTTS.from_pretrained(device="cuda")
+
+# Generate with Paralinguistic Tags
+text = "Hi there, Sarah here from MochaFone calling you back [chuckle], have you got one minute to chat about the billing issue?"
+
+# Generate audio (requires a reference clip for voice cloning)
+wav = model.generate(text, audio_prompt_path="soma.wav")
+
+ta.save("test-turbo.wav", wav, model.sr)
